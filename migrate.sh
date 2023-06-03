2cf4aca7e445db48f77b6193d5989ead411cf1e9
Стив <kenethra.74@gmail.com>
2023-06-03 17:13:54 -0600

    chore: remove print

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- docker-compose -f docker-compose.dev.yml run --rm -it python-s python manage.py makemigrations
+docker-compose -f docker-compose.dev.yml run --rm -it python-s python manage.py makemigrations
 docker-compose -f docker-compose.dev.yml run --rm -it python-s python manage.py migrate
--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse
 from rest_framework import status
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 from .models import CustomUser
@@ -30,7 +29,6 @@ class Login(APIView):
         email = request.data.get('email')
         password = request.data.get('password')
         user = authenticate(email=email, password=password)
-        print(user)
         if user:
             token_obj, _ = Token.objects.get_or_create(user=user)
             return Response({ 'status': 201, 'token': str(token_obj) }, status=status.HTTP_200_OK)
