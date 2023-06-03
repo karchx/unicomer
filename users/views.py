6b2c60e289aefe3863fe8b4a106661823913301e
Стив <kenethra.74@gmail.com>
2023-06-03 09:45:10 -0600

    feat: auth token

--- users/views.py
+++ users/views.py
@@ -1,16 +1,29 @@
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
 from rest_framework import status
 from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 from .models import CustomUser
 
 
-class RegisterUserAPIView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+class Register(APIView):
+    def post(self, request):
+        serializers = UserSerializer(data = request.data)
+
+        if not serializers.is_valid():
+            return Response({'status': 403, 'errors': serializers.errors,
+                             'message': 'Some wrong error'})
+
+        serializers.save()
+        
+        user = CustomUser.objects.get(email = serializers.data['email'])
+        token_obj, _ = Token.objects.get_or_create(user=user)
+
+        return Response({'status': 201, 'payload': serializers.data, 'token':
+                         str(token_obj), 'message':
+                         'User created'}, status=status.HTTP_201_CREATED)
 
 class LoginAPIView(APIView):
     def post(self, request):
